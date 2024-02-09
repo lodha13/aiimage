2f21fa5f80930380d80a214c04096d6c4cfa8ff2
Abhishek Lodha <abhishek_lodha13@yahoo.co.in>
2024-02-09 12:13:20 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -9,8 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-const allowOriginMiddleware = cors({origin: '*'})
-app.use(allowOriginMiddleware);
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
